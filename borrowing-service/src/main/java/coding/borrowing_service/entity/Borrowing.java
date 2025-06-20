efd771793c683177e1eea470913f0c9af2d62dc9
Sheruq <leniv.tech@gmail.com>
2025-06-20 13:07:10 +0300

    configurating docker-compose.yaml and Dockerfiles and kafka

--- borrowing-service/src/main/java/coding/borrowing_service/entity/Borrowing.java
+++ borrowing-service/src/main/java/coding/borrowing_service/entity/Borrowing.java
@@ -5,6 +5,7 @@ package coding.borrowing_service.entity;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.Instant;
@@ -14,6 +15,7 @@ import java.time.Instant;
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 @Table(name = "borrowings")
 public class Borrowing {
 
@@ -25,4 +27,12 @@ public class Borrowing {
     private Long readerId;
     private String action; // "BORROWED" or "RETURNED"
     private Instant timestamp;
+
+    public Borrowing(Long bookId, Long readerId, String action, Instant timestamp) {
+        this.bookId = bookId;
+        this.readerId = readerId;
+        this.action = action;
+        this.timestamp = timestamp;
+    }
+
 }
